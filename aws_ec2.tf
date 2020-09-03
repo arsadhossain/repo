0b7942f267013a1125e0e5e4e7f36fe024736657
arsadhossain <70659527+arsadhossain@users.noreply.github.com>
2020-09-04 00:32:10 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,18 +1,28 @@
 provider "aws" {
-        access_key = "XXXXXXXXXX"
-        secret_key = "XXXXXXXXXX"
+        access_key = "AKIATLGT42DGEXZXJEKT"
+        secret_key = "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"
         region     = "ap-south-1"
 }
 
 resource "aws_instance" "webserver" {
         ami = "ami-0ebc1ac48dfd14136"
         instance_type ="t2.micro"
-                key_name = "${aws_key_pair.ssh_key.key_name}"
+        key_name = "${aws_key_pair.ssh_key.key_name}"
         security_groups = ["${aws_security_group.allow_ports.name}"]
-    tags = {
-    name = "WebserverCG"
+        tags = {
+           Name = "ApacheWebServer"
         }
 }
+
+resource "aws_eip" "webservereip" {
+  vpc = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.webserver.id
+  allocation_id = aws_eip.webservereip.id
+}
+
 resource "aws_key_pair" "ssh_key" {
         key_name = "ssh_key"
         public_key = "${file("/home/ec2-user/.ssh/id_rsa.pub")}"
@@ -21,7 +31,7 @@ resource "aws_key_pair" "ssh_key" {
 resource "aws_security_group" "allow_ports" {
   name        = "allow_ports"
   description = "Allow traffic on port 22 and 80"
-  vpc_id      = "XXXXXXXXX"
+  vpc_id      = "vpc-ded136b5"
 
   ingress {
     from_port   = 80
@@ -51,4 +61,10 @@ resource "aws_security_group" "allow_ports" {
   tags = {
     Name = "allow_ports"
   }
+}
+output "instanceID" {
+  value = "${aws_instance.webserver.id}"
+}
+output "publicIP" {
+  value = "${aws_eip_association.eip_assoc.public_ip}"
 }
